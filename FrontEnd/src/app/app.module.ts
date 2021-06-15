b6a111e0da17f8494518ffcf571fb731101199df
Ashwin Rishi <ashwinrishipj@gmail.com>
2021-06-15 11:28:19 +0530

    login update 1.0

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './Student/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontEnd/src/app/login/login.component.ts
+++ FrontEnd/src/app/login/login.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   faUser = faUser;
+  loginForm: FormGroup;
+  unamePattern = "^[a-z0-9_-]{8,15}$";
+  pwdPattern = "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,12}$";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,private router: Router) {
+    this.loginForm = this.formBuilder.group(
+      {
+       username: ['',Validators.pattern(this.unamePattern)],
+       password: ['',Validators.pattern(this.pwdPattern)]
+      }
+    )
   }
 
-  public onInputOtp(event:any){
-   
+  ngOnInit(): void {}
+  onLoginSubmit(){
+    this.router.navigate(['/home']);
   }
-  public submitOtp(event : any){
-    event.preventDefault();
-    // alert(this.digitOne);
-  }
-
 }
